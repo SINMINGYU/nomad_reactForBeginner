273cf68f8c3733c6ce607bd609d5bbf5771c2334
FOX-P1 <smgms7513@gmail.com>
2022-12-29 15:28:31 +0900

    practice_08_useEffect

--- rfb-chapter-02/src/App.js
+++ rfb-chapter-02/src/App.js
@@ -1,11 +1,39 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+
+    console.log("i run all the time.");
+
+    useEffect(() => {
+        console.log("I run only once.");
+    }, []);
+    useEffect(() => {
+        console.log("I run 'couter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("I run 'couter' & 'keyword' changes.");
+    }, [counter, keyword]);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log(`SEARCH FOR ${keyword}`);
+        }
+    }, [keyword]);
+
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!!!</h1>
-            <Button text={"Continue"} />
+            <input
+                value={keyword}
+                onChange={onChange}
+                type={"text"}
+                placeholder="Search here..."
+            />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
         </div>
     );
 }
