b68cb79726f394296d1c17b963c76d8a238c8968
FOX-P1 <smgms7513@gmail.com>
2022-12-29 16:16:35 +0900

    practice_09_useEffect2

--- rfb-chapter-02/src/App.js
+++ rfb-chapter-02/src/App.js
@@ -1,39 +1,41 @@
 import { useEffect, useState } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    console.log("i run all the time.");
-
-    useEffect(() => {
-        console.log("I run only once.");
+function Hello() {
+    // 3개 다 같은 내용
+    /*
+    function byFn() {
+        console.log("Destroyed! :(");
+    }
+    const hiFn = () => {
+        console.log("Created! :)");
+        return byFn;
+    };
+    useEffect(hiFn, []); 
+    */
+    /*
+    useEffect(function () {
+        console.log("hi :)");
+        return function () {
+            console.log("bye :(");
+        };
     }, []);
+    */
+    /*
     useEffect(() => {
-        console.log("I run 'couter' changes.");
-    }, [counter]);
-    useEffect(() => {
-        console.log("I run 'couter' & 'keyword' changes.");
-    }, [counter, keyword]);
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log(`SEARCH FOR ${keyword}`);
-        }
-    }, [keyword]);
+        console.log("hi :)");
+        return () => console.log("bye :(");
+    }, []);
+    */
+    return <h1>Hello</h1>;
+}
 
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type={"text"}
-                placeholder="Search here..."
-            />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
