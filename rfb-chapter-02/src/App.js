213df236a51799bf5ddb7d49bcce3523e2d7f707
FOX-P1 <smgms7513@gmail.com>
2022-12-29 17:33:05 +0900

    practice_10_toDoList

--- rfb-chapter-02/src/App.js
+++ rfb-chapter-02/src/App.js
@@ -1,41 +1,39 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-    // 3개 다 같은 내용
-    /*
-    function byFn() {
-        console.log("Destroyed! :(");
-    }
-    const hiFn = () => {
-        console.log("Created! :)");
-        return byFn;
-    };
-    useEffect(hiFn, []); 
-    */
-    /*
-    useEffect(function () {
-        console.log("hi :)");
-        return function () {
-            console.log("bye :(");
-        };
-    }, []);
-    */
-    /*
-    useEffect(() => {
-        console.log("hi :)");
-        return () => console.log("bye :(");
-    }, []);
-    */
-    return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+
+    const onChange = (event) => {
+        setToDo(event.target.value);
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDo("");
+        setToDos((currentArray) => [toDo, ...currentArray]);
+    };
+
     return (
         <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={toDo}
+                    type={"text"}
+                    placeholder="Write your to do..."
+                    onChange={onChange}
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
     );
 }
